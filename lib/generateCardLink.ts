68778ae968658b3ff417ef94d66fdce8098d7a78
AmDeeJhay <amdeejhay@gmail.com>
2025-12-24 15:06:45 +0100

    refactor: Remove unused functions and types from the codebase

--- lib/generateCardLink.ts
+++
@@ -1,7 +0,0 @@
-export function generateCardLink(length = 8) {
-  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-  let id = ''
-  const values = crypto.getRandomValues(new Uint32Array(length))
-  for (let i = 0; i < length; i++) id += chars[values[i] % chars.length]
-  return id
-}
--- lib/validators.ts
+++
@@ -1,4 +0,0 @@
-export function validateFileType(file: File) {
-  const allowed = ['video/mp4', 'video/quicktime']
-  return allowed.includes(file.type)
-}
